a7b4681d0891ccce1dd7c5e76d4cbb3a0fc45486
dwcarey <danielwilliamcarey@gmail.com>
2023-04-25 14:05:55 +0800

    its working, delete and mark read/unread button functions added/fixed, styling adjusted slightly

--- js.js
+++ js.js
@@ -28,10 +28,41 @@ Book.prototype.info = function () {
 function addNewBook(title, author, pages, read, index) {
   const book = new Book(title, author, pages, read, index);
   myLibrary.push(book);
-  console.log(myLibrary);
+}
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+
+// hide and show differeetn submit/edit buttons in this method based on which button was clicked?
+function openForm() {
+  document.getElementById('formPopup').style.display = 'flex';
+  document.body.appendChild(overlay);
+}
+
+function closeForm() {
+  document.getElementById('formPopup').style.display = 'none';
+  document.body.removeChild(overlay);
 }
 
 const form = document.getElementById('bookForm');
+const cardHolder = document.querySelector('.cardHolder');
+const popupButton = document.getElementById('popupButton');
+
+popupButton.addEventListener('click', () => {
+  openForm();
+});
+
+function addToList(book) {
+  const bookInfo = `<div>
+  Title: ${book.title}
+  <br>Author: ${book.author}
+  <br>Pages: ${book.pages}
+  <br>Read: ${book.read}
+  <br>Index: ${book.index}
+  <br><Button id="deleteButton-${book.index}">Delete</Button>
+  <Button id="editButton-${book.index}">Mark as Read/Unread</Button>
+  </div>`;
+  cardHolder.insertAdjacentHTML('beforeend', bookInfo);
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -47,47 +78,64 @@ form.addEventListener('submit', (e) => {
   closeForm();
 });
 
-const cardHolder = document.querySelector('.cardHolder');
+function deleteButton(index) {
+  myLibrary.splice(index, 1);
+  cardHolder.replaceChildren();
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    myLibrary[i].index = i;
+    const bookInfo = `<div>
+    Title: ${myLibrary[i].title}
+    <br>Author: ${myLibrary[i].author}
+    <br>Pages: ${myLibrary[i].pages}
+    <br>Read: ${myLibrary[i].read}
+    <br>Index: ${myLibrary[i].index}
+    <br><Button id="deleteButton-${myLibrary[i].index}">Delete</Button>
+    <Button id="editButton-${myLibrary[i].index}">Mark as Read/Unread</Button>
+    </div>`;
 
-// this aint right
-function addToList(book) {
-  const bookInfo = `<div>Title: ${book.title}<br>Author: ${book.author}<br>Pages: ${book.pages}<br>Read: ${book.read}<br>Index: ${book.index}<br><button id="deleteButton-${book.index}">Delete</Button></div>`;
-  cardHolder.insertAdjacentHTML('beforeend', bookInfo);
+    cardHolder.insertAdjacentHTML('beforeend', bookInfo);
+  }
 }
 
 cardHolder.addEventListener('click', (e) => {
   if (e.target && e.target.id.startsWith('deleteButton-')) {
     const index = e.target.id.split('-')[1];
-    console.log(`Delete button ${index} clicked`);
-
     deleteButton(index);
   }
 });
 
-function deleteButton(index) {
-  myLibrary.splice(index, 1);
+function editButtonClicked(index) {
+  const editEntry = myLibrary.splice(index, 1)[0];
+  if (editEntry.read === 'YES') {
+    editEntry.read = 'NO';
+  } else {
+    editEntry.read = 'YES';
+  }
+  myLibrary.splice(index, 0, editEntry);
+
   cardHolder.replaceChildren();
   for (let i = 0; i < myLibrary.length; i++) {
     myLibrary[i].index = i;
-    const bookInfo = `<div>Title: ${myLibrary[i].title}<br>Author: ${myLibrary[i].author}<br>Pages: ${myLibrary[i].pages}<br>Read: ${myLibrary[i].read}<br>Index: ${myLibrary[i].index}<br><button id="deleteButton-${myLibrary[i].index}">Delete</Button></div>`;
+    const bookInfo = `<div>
+    Title: ${myLibrary[i].title}
+    <br>Author: ${myLibrary[i].author}
+    <br>Pages: ${myLibrary[i].pages}
+    <br>Read: ${myLibrary[i].read}
+    <br>Index: ${myLibrary[i].index}
+    <br><Button id="deleteButton-${myLibrary[i].index}">Delete</Button>
+    <Button id="editButton-${myLibrary[i].index}">Mark as Read/Unread</Button>
+    </div>`;
+
     cardHolder.insertAdjacentHTML('beforeend', bookInfo);
   }
-  const deletedButton = document.getElementById(`deleteButton-${index}`);
-  deletedButton.removeAttribute('onclick');
 }
 
-const overlay = document.createElement('div');
-overlay.classList.add('overlay');
-
-function openForm() {
-  document.getElementById('formPopup').style.display = 'flex';
-  document.body.appendChild(overlay);
-}
-
-function closeForm() {
-  document.getElementById('formPopup').style.display = 'none';
-  document.body.removeChild(overlay);
-}
+cardHolder.addEventListener('click', (e) => {
+  if (e.target && e.target.id.startsWith('editButton-')) {
+    const index = e.target.id.split('-')[1];
+    editButtonClicked(index);
+  }
+});
 
 const closeButton = document.getElementById('closeButton');
 
